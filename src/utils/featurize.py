729ebc9ba616ec391194c27141757838e0cddbe0
saijithendr <jithenkumar1@gmail.com>
2022-10-22 14:47:17 +0200

    stage 03 py scripting done

---
+++ src/utils/featurize.py
@@ -0,0 +1,16 @@
+import logging
+import joblib
+import numpy as np
+import scipy.sparse as sparse
+
+
+def save_matrix(df, matrix, out_path):
+    id_matrix = sparse.csr_matrix(df.id.astype(np.int64)).T
+    label_matrix = sparse.csr_matrix(df.label.astype(np.int64)).T
+
+    result = sparse.hstack([id_matrix, label_matrix, matrix], format='csr')
+
+    joblib.dump(result, out_path)
+    msg = f'The output matrix saved at: {out_path} of the size: {result.shape} and data type: {result.dtype}\n'
+    logging.info(msg)
+    
